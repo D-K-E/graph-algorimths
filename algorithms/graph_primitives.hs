a6bf76b2a7cddc1f9279cf1df4886fefd80d23d1
kaaneraslan <kaaneraslan@gmail.com>
2018-04-15 12:18:55 +0200

    restructured some primitives

--- algorithms/graph_primitives.hs
+++ algorithms/graph_primitives.hs
@@ -7,20 +7,23 @@ License     :  Mit
 Maintainer  :  Kaan
 Stability   :  unstable
 Portability :  don't know
-
-
 -}
 
+-- TODO See functions, we should work with identifiers instead of
+-- the data itself, for not consuming a lot of memory
 
 -- Module for graph primitives
 
+-- Data Declarations
+
+-- Vertex
 
 data Vertex = Vertex {
-  inIds :: VertexIDs
-  , vertexId :: VertexID
-  , vertexData :: VDataL
-  , outIds :: VertexIDs
-  } deriving (Show)
+  getInIds :: VertexIDs
+  , getVertexId :: VertexID
+  , getVertexData :: VDataL
+  , getOutIds :: VertexIDs
+  } deriving (Show, Eq)
 
 data VData = String
            | Integer
@@ -28,40 +31,95 @@ data VData = String
            | Char
            | Bool
            | Double
-           deriving(Show)
+           deriving(Show, Eq)
 
 data VertexID = VertexID Integer
+              deriving(Show, Eq, Ord)
+
+data VertexIDs = VertexIDs{
+  getVertexIds :: [VertexID]
+  }   deriving(Show, Eq, Ord)
+
+data VDataL = VDataL{
+  getVertexDataL :: [VData]
+  } deriving(Show, Eq)
+
+data Vertices = Vertices [Vertex]
               deriving(Show, Eq)
 
-data VertexIDs = VertexIDs [VertexID]
-               deriving(Show, Ord, Eq)
+-- Edge
 
-data VDataL = VDataL [VData]
-            deriving(Show, Ord, Eq)
+data Edge = Edge {
+  getEdgeData :: (Vertex, Vertex)
+  , getEdgeId :: EdgeID
+  , getEdgeFuncs :: FuncIDs
+  } deriving(Show, Eq)
 
-data Edge = Edge (Vertex, Vertex)
+data EdgeID = EdgeID Integer
+            deriving(Show, Eq)
 
-data Graph = Graph ([Vertex], [Edge])
+data EdgeIDs = EdgeIDs [EdgeID]
+             deriving(Show, Eq)
+
+-- Functions for mapping functions to edges
+
+data FuncID = FuncID Integer
+            deriving(Show, Eq, Ord)
+
+data FuncIDs = FuncIDs{
+  getFuncIds :: [FuncID]
+  } deriving(Show, Eq, Ord)
+
+data Func = Func{
+  getFuncId :: FuncID
+  , getFuncParams :: VDataL
+  }
+
+-- data FuncData = FuncData FuncId Func
+
+-- Graph
+
+data Graph = Graph (VertexIDs, EdgeIDs)
+
+-- Function declarations
+
+-- Function Types
 
 -- Edge related functions
 
 getFirstV :: Edge -> Vertex
-
-getFirstV anEdge = fst anEdge
-
 getSecondV :: Edge -> Vertex
-getSecondV anEdge = snd anEdge
-
 getV_e :: Edge -> Int -> Vertex
+getVs_e :: Edge -> [Vertex]
+getVs_es :: [Edge] -> [[Vertex]]
+getVList_es :: [Edge] -> [Vertex]
+
+
+-- Vertex Related Functions
+
+getInEdges :: Vertex -> [Edge]
+
+
+-- Graph Related Functions
+
+getOrder :: Graph -> Integer
+isTrivial :: Graph -> Bool
+
+-- Function Implementations
+
+-- Edge related functions
+
+getFirstV anEdge = fst (getEdgeData anEdge)
+
+getSecondV anEdge = snd (getEdgeData anEdge)
+
 
 getV_e anEdge 1 = getFirstV anEdge
 getV_e anEdge 2 = getSecondV anEdge
 
-getVs_e :: Edge -> [Vertex]
 
 getVs_e anEdge = (getV_e anEdge 1):(getV_e anEdge 2):[]
 
-getVs_es :: [Edge] -> [[Vertex]]
 
 getVs_es edgeList = if null edgeList
                           then []
@@ -69,22 +127,19 @@ getVs_es edgeList = if null edgeList
 
 getVsFromEdges [] = []
 
-getVList_es :: [Edge] -> [Vertex]
+--makeGraphFromEdges :: [Edge] -> Graph
 
 getVList_es edgeList = concat (getVs_es edgeList)
 
-makeGraphFromEdges :: [Edge] -> Graph
+-- Vertex Related Functions
 
--- General Graph functions --
-
-isTrivial :: Graph -> Bool
+-- Graph Related Functions
 
 isTrivial aGraph = if null (fst aGraph)
                       then True
                       else False
 
-getOrder :: Graph -> Integer
 
-getOrder aGraph = if isEmpthGraph aGraph
+getOrder aGraph = if isTrivial aGraph
                   then 0
                   else length (fst aGraph)
